1dfb9f285f64a264d9d20755c95b199d3cda7be7
sheynd <sheynvip16@gmail.com>
2016-11-20 15:00:55 -0500

    First dummy code commit

---
+++ MarsAgent.java
@@ -0,0 +1,16 @@
+public class MarsAgent extends Agent {
+	public MarsAgent(String name, String team) {
+		super(name, team);
+		//TODO add anything if needed
+	}
+
+	@Override
+	public void handlePercept(Percept p) {
+		//TODO handle the necessary percepts
+	}
+
+	public Action step() {
+		//TODO deliberate and return an action
+		return null;
+	}
+}
