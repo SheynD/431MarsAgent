d7f18a464ac6bd85ca6bcf5f98100cc84ae1f0a0
tal215 <tal215@lehigh.edu>
2016-12-05 12:02:42 -0500

    update ExploreStrategy

--- com/cse431/marsmen/strategy/ExploreStrategy.java
+++ com/cse431/marsmen/strategy/ExploreStrategy.java
@@ -1,11 +1,51 @@
 package com.cse431.marsmen.strategy;
 
 import com.cse431.marsmen.MarsAgent;
+import java.util.ArrayList;
+import java.util.Collections;
+
+import apltk.interpreter.data.LogicBelief;
 import eis.iilang.Action;
+import massim.javaagents.agents.MarsUtil;
+
 
 public class ExploreStrategy implements Strategy{
-
-	public Action execute (MarsAgent agent) {
-		return null;
+	public Action execute(MarsAgent agent) {
+		if (!agent.getAllBeliefs("role").getFirst().getParameters().get(0).equals("Explorer")) {
+            return null;
+        }
+        if (Integer.parseInt(agent.getAllBeliefs("health").getFirst().getParameters().get(0)) == 0) {
+            return null;
+        }
+        if (agent.containsBelief(new LogicBelief("probedVertex", agent.getAllBeliefs("position").getFirst().getParameters().get(0)))){
+        	//goto a different vertex
+        	Util u = new Util(agent);
+        	ArrayList <LogicBelief> edges = new ArrayList<>();
+        	for (LogicBelief l : agent.getAllBeliefs("surveyedEdge")){
+        		if (l.getParameters().get(0).equals( agent.getAllBeliefs("position").getFirst().getParameters().get(0)) || l.getParameters().get(1).equals( agent.getAllBeliefs("position").getFirst().getParameters().get(0)) ){
+        			edges.add(l);
+        		}
+        	}
+        	if (edges.isEmpty()){
+        		return MarsUtil.surveyAction();
+        	}
+        	else{
+        		ArrayList <String> verticies = u.getNeighborVertexes(agent.getAllBeliefs("position").getFirst().getParameters().get(0));
+        		Collections.shuffle(verticies);
+        		for (String v : verticies){
+        			if (agent.containsBelief(new LogicBelief("probedVertex", v))){
+        				continue;
+        			}
+        			else {
+        				return MarsUtil.gotoAction(v);
+        			}
+        		}
+        		return MarsUtil.gotoAction(verticies.get(0));
+        	}
+        }
+        else {
+        	return MarsUtil.probeAction();
+        }
+        
 	}
 }
