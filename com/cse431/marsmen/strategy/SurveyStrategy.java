2c55643bbd6db8b1cad7cb84b863939c9a21762f
tal215 <tal215@lehigh.edu>
2016-12-05 12:04:29 -0500

    update simple SurveyStrategy

--- com/cse431/marsmen/strategy/SurveyStrategy.java
+++ com/cse431/marsmen/strategy/SurveyStrategy.java
@@ -1,11 +1,31 @@
 package com.cse431.marsmen.strategy;
 
 import com.cse431.marsmen.MarsAgent;
+import java.util.ArrayList;
+import java.util.Collections;
+
+import apltk.interpreter.data.LogicBelief;
 import eis.iilang.Action;
+import massim.javaagents.agents.MarsUtil;
 
 public class SurveyStrategy implements Strategy{
 
-	public Action execute (MarsAgent agent) {
-		return null;
+	public Action execute(MarsAgent agent) {
+		if (Integer.parseInt(agent.getAllBeliefs("health").getFirst().getParameters().get(0)) == 0) {
+            return null;
+        }
+		ArrayList <LogicBelief> edges = new ArrayList<>();
+    	for (LogicBelief l : agent.getAllBeliefs("surveyedEdge")){
+    		if (l.getParameters().get(0).equals( agent.getAllBeliefs("position").getFirst().getParameters().get(0)) || l.getParameters().get(1).equals( agent.getAllBeliefs("position").getFirst().getParameters().get(0)) ){
+    			edges.add(l);
+    		}
+    	}
+    	if (edges.isEmpty()){
+    		return MarsUtil.surveyAction();
+    	}
+    	else {
+    		return null;
+    	}
 	}
+	
 }
