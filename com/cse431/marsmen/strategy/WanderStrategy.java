150b3017351dfb0e46dace4285bf97f400a4328f
tal215 <tal215@lehigh.edu>
2016-12-06 12:20:03 -0500

    update wander strategy

--- com/cse431/marsmen/strategy/WanderStrategy.java
+++ com/cse431/marsmen/strategy/WanderStrategy.java
@@ -6,6 +6,9 @@ import eis.iilang.Action;
 public class WanderStrategy implements Strategy{
 
 	public Action execute (MarsAgent agent) {
-		return null;
+		Util u = new Util(agent);
+		ArrayList <String> verticies = u.getNeighborVertexes(agent.getAllBeliefs("position").getFirst().getParameters().get(0));
+		Collections.shuffle(verticies);
+		return MarsUtil.gotoAction(verticies.get(0));
 	}
 }
