1f5ec928b6c2b0527a9d3a0dc94f625a7804e841
tal215 <tal215@lehigh.edu>
2016-12-06 12:46:39 -0500

    fixed some errors in reedRepair

--- com/cse431/marsmen/strategy/NeedRepairStrategy.java
+++ com/cse431/marsmen/strategy/NeedRepairStrategy.java
@@ -17,8 +17,11 @@ public class NeedRepairStrategy implements Strategy{
 		if (agent.getAllBeliefs("position").getFirst().getParameters().get(0).isEmpty()){
 			return null;
 		}
-		if (agent.getAllBeliefs("repairComing").getFirst().getParameters().get(0).equals(agent.getAllBeliefs("position").getFirst().getParameters().get(0))){
-			return null;
+		if (!agent.getAllBeliefs("repairComing").isEmpty()) {
+			if (agent.getAllBeliefs("repairComing").getFirst().getParameters().get(0)
+					.equals(agent.getAllBeliefs("position").getFirst().getParameters().get(0))) {
+				return null;
+			}
 		}
 		if (Integer.parseInt(agent.getAllBeliefs("health").getFirst().getParameters().get(0)) == 0){
 			agent.broadcastBelief(new LogicBelief("needRepair", agent.getAllBeliefs("position").getFirst().getParameters().get(0)));
