8a8198527d42255066e90730732e3ece725820af
tal215 <tal215@lehigh.edu>
2016-12-08 14:43:32 -0500

    changed team names in config

--- com/cse431/marsmen/strategy/NaiveRepairStrategy.java
+++ com/cse431/marsmen/strategy/NaiveRepairStrategy.java
@@ -21,10 +21,17 @@ public class NaiveRepairStrategy implements Strategy {
         String entity = "";
         /* Loop thru all visible entities */
 		for (LogicBelief visibleEntity : agent.getAllBeliefs("visibleEntity", agent.getName())){
+			System.err.println("VisibleEntity: " + visibleEntity.getParameters().get(0) + " " + visibleEntity.getParameters().get(1) + " " + visibleEntity.getParameters().get(2) + " " + visibleEntity.getParameters().get(3) + " " + visibleEntity.getParameters().get(4));
+			System.err.println("agent: " + agent.getName() + " " + agent.getTeam());
+			for (String l : u.getNeighborVertexes(agent.getAllBeliefs("position", "", agent.getName()).getFirst().getParameters().get(0))){
+				System.err.println(l);
+			}
 			if (agent.getTeam().equals(visibleEntity.getParameters().get(3)) && /* If they are on out team */
 					visibleEntity.getParameters().get(4).equals("disabled") &&  /* And they are disabled */
-					u.getNeighborVertexes(agent.getAllBeliefs("position").getFirst().getParameters().get(0))
+					u.getNeighborVertexes(agent.getAllBeliefs("position", "", agent.getName()).getFirst().getParameters().get(0))
 					.contains(visibleEntity.getParameters().get(2))){ /* And they are a neighbor */
+				//System.err.println(visibleEntity.getParameters().get(0) + " " + visibleEntity.getParameters().get(1) + " " + visibleEntity.getParameters().get(2) + " " + visibleEntity.getParameters().get(3) + " " + visibleEntity.getParameters().get(4));
+
 				entity = visibleEntity.getParameters().get(1);
 				/* Shouldn't we just wait for the next round of percepts to update our beliefs? */
 				agent.removeBeliefs("visibleEntity", visibleEntity.getParameters().get(0), 
